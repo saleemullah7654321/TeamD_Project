9bf2fe06f09cc647329a9c33ada899d098d63ac0
saleemullah <saleemullah7654321@gmail.com>
2021-11-09 15:26:53 +0500

    pull

--- workingFile.py
+++ workingFile.py
@@ -1,3 +1,5 @@
+
+
 import re
 def count_evens(a):
     b=[i for i in a if i%2==0]
@@ -68,27 +70,5 @@ def get_depth(d):
                 else:
                     v_check(v)
     check(d)
-
+    
     return o
-def validate_password(password):
-    def validate_length(password):
-        if len(password)>=6:
-            return True
-        return False
-    def validate_symbol(password):
-        if "@" in password or "$" in password or "#" in password:
-            return True
-        return False
-    def validate_lower(password):
-        return any([True for x in password if "a"<=x<="z"])
-    def validate_upper(password):
-        return any([True for x in password if "A"<=x<="Z"])
-    def validate_digit(password):
-        return any([True for x in password if x.isnumeric()])
-    return all([
-        validate_length(password),
-        validate_symbol(password),
-        validate_lower(password),
-        validate_upper(password),
-        validate_digit(password)
-    ])
